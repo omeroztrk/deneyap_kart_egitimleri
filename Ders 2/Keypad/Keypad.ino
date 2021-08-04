35b30786443c6174b2d9bdb6ea3651c3bfc227c3
omeroztrk <62025609+omeroztrk@users.noreply.github.com>
2021-08-04 14:43:56 +0300

    Update Keypad.ino

--- Ders 2/Keypad/Keypad.ino
+++ Ders 2/Keypad/Keypad.ino
@@ -1,18 +1,18 @@
 #include "Keypad.h"
 #include "deneyap.h"
 
-const byte satir = 4;         // dört satır
-const byte sutun = 4;         // dört sütun
+const byte satir = 4;     // dört satır
+const byte sutun = 3;     // üç sütun
 
 char tus_takimi[satir][sutun] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
 };
 
-byte satir_pinleri[satir] = {D3, D4, D5, D6};     //satır pinleri
-byte sutun_pinleri[sutun] = {D12, D13, D14, D15};     //sütun pinleri
+byte satir_pinleri[satir] = {D3, D4, D5, D6};  //satır pinleri
+byte sutun_pinleri[sutun] = {D12, D13, D14};  //sütun pinleri
 
 Keypad deneyap_keypad = Keypad( makeKeymap(tus_takimi), satir_pinleri, sutun_pinleri, satir, sutun );
 void setup() {
