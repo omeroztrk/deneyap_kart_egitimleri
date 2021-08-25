12a45469ae1b44ccd48f0ec7a3e0b2dd522b2e80
omeroztrk <62025609+omeroztrk@users.noreply.github.com>
2021-08-25 10:39:08 +0300

    Update Keypad.ino

--- Ders 2/Keypad/Keypad.ino
+++ Ders 2/Keypad/Keypad.ino
@@ -1,28 +1,33 @@
+#include <deneyap.h>
 #include "Keypad.h"
-#include "deneyap.h"
 
-const byte satir = 4;     // dört satır
-const byte sutun = 3;     // üç sütun
-
-char tus_takimi[satir][sutun] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
+char tus_takimi[4][3] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}  
 };
 
-byte satir_pinleri[satir] = {D3, D4, D5, D6};  //satır pinleri
-byte sutun_pinleri[sutun] = {D12, D13, D14};  //sütun pinleri
 
-Keypad deneyap_keypad = Keypad( makeKeymap(tus_takimi), satir_pinleri, sutun_pinleri, satir, sutun );
+byte satir_pinleri[4] = {D3, D4, D5, D6};
+byte sutun_pinleri[3] = {D12, D13, D14};
+
+Keypad deneyap_keypad = Keypad(
+    makeKeymap(tus_takimi),
+    satir_pinleri,
+    sutun_pinleri,
+    4,
+    3
+  );
+
 void setup() {
-Serial.begin(115200);      // Seri portu başlat
+  Serial.begin(115200);
 }
 
+
 void loop() {
-  char tus = deneyap_keypad.getKey();  
-    if (tus != NO_KEY)
-  {
-    Serial.println(tus);
+  char tus = deneyap_keypad.getKey();
+  if(tus != NO_KEY){
+    Serial.println(tus);  
   }
 }
