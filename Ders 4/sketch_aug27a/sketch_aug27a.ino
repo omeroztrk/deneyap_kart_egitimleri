d953f972bb963aed64255495980bc1969c6a3de7
omeroztrk <omersaid@sabanciuniv.edu>
2021-08-27 10:50:44 +0300

    Temp IOt

--- Ders 4/sketch_aug27a/sketch_aug27a.ino
+++ Ders 4/sketch_aug27a/sketch_aug27a.ino
@@ -3,15 +3,41 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 
+#include "lsm6dsm.h"
+
+LSM6DSM imu;
+
 WebServer server(80);
 
 void anasayfa(){
   Serial.println("Ana sayfaya gelindi");
 
-  server.send(200, "text/plain", "Ana Sayfaya Hosgeldin");
+  server.send(200, "text/html", 
+"<!DOCTYPE HTML>\n"
+"<html>\n"
+"<head>\n"
+"<title>Sicaklik degeri</title>\n"
+"</head>\n"
+"<body>\n"
+"<h1>Hosgeldiniz</h1>\n"
+"<p>Sicaklik Degeri: \n"
+"" + String( imu.readTempC() )  + " derece\n"
+"</p>\n"
+"</body>\n"
+"\n"
+"<script>\n"
+"setTimeout(function(){\n"
+"   window.location.reload(1);\n"
+"}, 1000);\n"
+"</script>\n"
+"</html>\n"
+""
+  );
 }
 
 void setup(){
+  imu.begin();
+  
   Serial.begin(115200);
   delay(100);
 
@@ -20,8 +46,7 @@ void setup(){
 
   while(WiFi.status() != 3){
     Serial.print(".");
-
-      delay(500);
+    delay(500);
   }
 
   Serial.println("");
